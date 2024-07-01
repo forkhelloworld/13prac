da7ca2e5c704966a569abf7303338a2ec48cd22c
kurisu <programasonika@gmail.com>
2024-07-01 17:53:41 +0300

    Finished the task

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-alert("Hello World")
+const a = 5;
+const b = 23;
+console.log(b * a);
+
+const c = 9;
+const d = 3;
+console.log(c / d);
+
+const e = 4;
+const f = 11;
+console.log(e + f);
+
+const eleven = 11;
+const truth = true;
+const bestProgrammingLanguage = "Java Script";
+const oneHundredString = "100";
+console.log(eleven, truth, bestProgrammingLanguage, oneHundredString);
+
+let num = 1;
+num += 11;
+num -= 11;
+num *= 11;
+num /= 11;
+num++;
+num--;
+
+console.log(Number(prompt()) ** 2);
+
+console.log((Number(prompt()) + Number(prompt())) / 2);
+
+console.log(Number(prompt()) * 60);
+
+let greeting = "Hello "
+let userName;
+userName = prompt()
+console.log(greeting + userName);
